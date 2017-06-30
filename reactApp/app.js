e99833737c15f22951bc6d85cfda34dde98d5d4e
segalb <none>
2017-06-30 14:31:04 -0700

    finished step 2

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var dummyData=["clean","brush","buy","build"];
+var dummyData=[{taskText : "clean", completed:false},
+{taskText : "build", completed:false},
+{taskText : "manage", completed:true},
+{taskText : "renovate", completed:false}];
+
 
 class Todo extends React.Component {
   constructor(props){
@@ -9,8 +13,7 @@ class Todo extends React.Component {
   }
   render(){
     return(
-      <li key={this.props.index}> <button type="button">x</button> {this.props.item}</li>
-
+      <li  key={this.props.index} style={ this.props.completed ? {textDecoration:"line-through"}:{} }> <button type="button">x</button> {this.props.item}</li>
     );
   }
 }
@@ -22,8 +25,43 @@ class TodoList extends React.Component {
   render(){
     return(
       <ul>
-      {dummyData.map((data,i)=> {return <Todo item={data} index={i}/>})}
+      {this.props.todos.map((data,i)=> {return <Todo item={data.taskText} completed={data.completed} index={i}/>})}
     </ul>);
+  }
+}
+
+class InputLine extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>
+      <input type="text" placeholder="task" ></input>
+      <button type="button">x</button>
+    </div>
+    );
+  }
+}
+
+class TodoApp extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      todos:[],
+    };
+  }
+
+  componentDidMount (){
+    this.setState({todos: dummyData})
+  }
+  render(){
+    return(
+      <div>
+      <InputLine  />
+    <TodoList todos={this.state.todos}/>
+</div>
+    );
   }
 }
 
@@ -31,4 +69,5 @@ class TodoList extends React.Component {
 
 
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+
+ReactDOM.render(<TodoApp />, document.getElementById('root'));

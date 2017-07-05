32ace07c35aa9e1128cf71bfc62b7c2c3f71b6a0
segalb <none>
2017-07-05 08:58:02 -0700

    friday

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+var express = require('express') ;
+//var dbRoutes = require('../routes/databaseAccess.js') ;
+const app = express();
+
+// This line makes the build folder publicly available.
+app.use(express.static('build'));
+
+//app.use('/db', dbRoutes);
+
+app.listen(3000, () => {
+  console.log('Server for React Todo App listening on port 3000!')
+});
